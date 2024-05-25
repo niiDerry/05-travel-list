c5ee7e63573c2c1d16ddec1aac930690277aa4a8
Nii Derry <derricktagoe@gmail.com>
2024-05-25 15:04:00 +0000

    separated components into separate files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { useState } from "react";
-
-// const initialItems = [
-//   { id: 1, description: "Passports", quantity: 2, packed: false },
-//   { id: 2, description: "Socks", quantity: 12, packed: true },
-//   { id: 3, description: "Charger", quantity: 1, packed: false },
-// ];
+import Logo from "./components/Logo";
+import Form from "./components/Form";
+import PackingList from "./components/PackingList";
+import Stats from "./components/Stats";
 
 export default function App() {
   // This state was lifted from the form component
   const [items, setItems] = useState([]);
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  const [sortBy, setSortBy] = useState("input");
 
   function handleAddItems(item) {
     // adding items to the list
@@ -19,92 +21,45 @@ export default function App() {
     setItems((items) => items.filter((item) => item.id !== id));
   }
 
-  return (
-    <div className="app">
-      <Logo />
-      {/* a new prop onAddItems was created */}
-      <Form onAddItems={handleAddItems} />
-      <PackingList items={items} onDeleteItem={handleRemoveItem} />
-      <Stats />
-    </div>
-  );
-}
+  function handleToggleItem(id) {
+    // this is how to update an array of items
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
 
-function Logo() {
-  return <h1>🏖️ Far Away 👜</h1>;
-}
-
-function Form({ onAddItems }) {
-  const [description, setDescription] = useState("");
-  const [quantity, setQuantity] = useState(1);
-
-  function handleSubmit(e) {
-    e.preventDefault();
-
-    if (!description) return;
-
-    const newItem = { description, quantity, packed: false, id: Date.now() };
-    console.log(newItem);
-
-    onAddItems(newItem);
-
-    setDescription("");
-    setQuantity(1);
+  function handleClearItems() {
+    const confirmed = window.confirm(
+      "Are you sure you want to clear this list?"
+    );
+    if (confirmed) setItems([]);
   }
 
   return (
-    <form className="add-form" onSubmit={handleSubmit}>
-      <h3>What do you need for your 😍 trip?</h3>
-      <select
-        value={quantity}
-        onChange={(e) => setQuantity(Number(e.target.value))}
-      >
-        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
-          <option value={num} key={num}>
-            {num}
-          </option>
-        ))}
-      </select>
-      <input
-        type="text"
-        placeholder="Item..."
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
+    <div className="app">
+      <Logo />
+
+      {/* a new prop onAddItems was created */}
+      <Form
+        onAddItems={handleAddItems}
+        description={description}
+        quantity={quantity}
+        setDescription={setDescription}
+        setQuantity={setQuantity}
       />
-      <button>Add</button>
-    </form>
-  );
-}
 
-function PackingList({ items, onDeleteItem }) {
-  return (
-    <div className="list">
-      <ul>
-        {items.map((item) => (
-          <Item item={item} onDeleteItem={onDeleteItem} key={item.id} />
-        ))}
-      </ul>
+      <PackingList
+        items={items}
+        sortBy={sortBy}
+        setSortBy={setSortBy}
+        onDeleteItem={handleRemoveItem}
+        onToggleItem={handleToggleItem}
+        onClearItems={handleClearItems}
+      />
+
+      <Stats items={items} />
     </div>
-  );
-}
-
-function Item({ item, onDeleteItem }) {
-  return (
-    <li>
-      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
-        {item.quantity} {item.description}
-      </span>
-      <button onClick={() => onDeleteItem(item.id)}>❌</button>
-    </li>
-  );
-}
-
-function Stats() {
-  return (
-    <footer className="stats">
-      <em>
-        👜 You have X items on your list, and you have already packed X - (X%)✈️
-      </em>
-    </footer>
   );
 }
